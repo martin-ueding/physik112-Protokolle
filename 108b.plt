34a2ac33e10b0e789da5831d1838cb17ad678798
Martin Ueding <dev@martin-ueding.de>
2012-04-19 18:22:52 +0200

    Finish, or something

--- 108b.plt
+++ 108b.plt
@@ -5,11 +5,11 @@ set encoding utf8
 
 # Set output to a nice PDF.
 set terminal postscript eps enhanced color solid defaultplex "Helvetica" 12
-set output "|epstopdf --filter > plot.pdf"
+set output "|epstopdf --filter > plot_108b.pdf"
 
 # Label the axes.
-set xlabel "Fallhöhe [m]"
-set ylabel "omega^2 [1/s^2]"
+set xlabel "Kreisfrequenz^2 [1/s^2]"
+set ylabel "Fallhöhe [m]"
 
 # Enable the grid.
 set grid
@@ -17,11 +17,66 @@ set grid
 # Set the title of the whole graph.
 set title "physik111 Versuch 108: Aufgabe 108.b"
 
+f25_25(x) = a * x + b;
+f25_100(x) = c * x + d;
+f50_25(x) = e * x + f;
+f50_100(x) = g * x + h;
+
+fit f25_25(x) "108b_25_25.dat" using 1:2 via a,b
+fit f25_100(x) "108b_25_100.dat" using 1:2 via c,d
+fit f50_25(x) "108b_50_25.dat" using 1:2 via e,f
+fit f50_100(x) "108b_50_100.dat" using 1:2 via g,h
+
 # Plot the data with the fit.
 plot "108b_25_25.dat" title "25 g, 2.5 cm", \
 	 "108b_25_100.dat" title "25 g, 10 cm", \
 	 "108b_50_25.dat" title "50 g, 2.5 cm", \
-	 "108b_50_100.dat" title "50 g, 10 cm"
+	 "108b_50_100.dat" title "50 g, 10 cm", \
+	 f25_25(x) title "Fit 25 g, 2.5 cm", \
+	 f25_100(x) title "Fit 25 g, 10 cm", \
+	 f50_25(x) title "Fit 50 g, 2.5 cm", \
+	 f50_100(x) title "Fit 50 g, 10 cm"
+
+# Set the output again so that the PDF is really written to.
+set output
+
+###############################################################################
+#                                    108.c                                    #
+###############################################################################
+
+# Set output to a nice PDF.
+set terminal postscript eps enhanced color solid defaultplex "Helvetica" 12
+set output "|epstopdf --filter > plot_108c.pdf"
+
+# Label the axes.
+set xlabel "Kreisfrequenz [1/s]"
+set ylabel "Fallzeit [s]"
+
+# Enable the grid.
+set grid
+
+# Set the title of the whole graph.
+set title "physik111 Versuch 108: Aufgabe 108.b"
+
+g25_25(x) = a * x + b;
+g25_100(x) = c * x + d;
+g50_25(x) = e * x + f;
+g50_100(x) = g * x + h;
+
+fit g25_25(x) "108c_25_25.dat" using 1:2 via a,b
+fit g25_100(x) "108c_25_100.dat" using 1:2 via c,d
+fit g50_25(x) "108c_50_25.dat" using 1:2 via e,f
+fit g50_100(x) "108c_50_100.dat" using 1:2 via g,h
+
+# Plot the data with the fit.
+plot "108c_25_25.dat" title "25 g, 2.5 cm", \
+	 "108c_25_100.dat" title "25 g, 10 cm", \
+	 "108c_50_25.dat" title "50 g, 2.5 cm", \
+	 "108c_50_100.dat" title "50 g, 10 cm", \
+	 g25_25(x) title "Fit 25 g, 2.5 cm", \
+	 g25_100(x) title "Fit 25 g, 10 cm", \
+	 g50_25(x) title "Fit 50 g, 2.5 cm", \
+	 g50_100(x) title "Fit 50 g, 10 cm"
 
 # Set the output again so that the PDF is really written to.
 set output
--- traegheitsmoment.m
+++ traegheitsmoment.m
@@ -5,62 +5,157 @@
 #                  physik111 Versuch 108 „Trägheitsmoment“                    #
 ###############################################################################
 
-# Lade Daten.
-#
+I_theo = 0.0084891;
+
+###############################################################################
+#                                 Daten laden                                 #
+###############################################################################
+
 # Struktur der Spalten in ``data``:
 #
 # Masse Radius Fallhöhe Fallzeit Umläufe Umlaufszeit
 load "data.dat"
 
 # Zerlege die Datentabelle in einzelne Spaltenvektoren.
-massen = data(:, 1)
-radien = data(:, 2)
-hoehen = data(:, 3)
-fallzeiten = data(:, 4)
-umlaeufe = data(:, 5)
-umlaufzeit = data(:, 6)
+massen = data(:, 1);
+radien = data(:, 2);
+hoehen = data(:, 3);
+fallzeiten = data(:, 4);
+umlaeufe = data(:, 5);
+umlaufzeit = data(:, 6);
 
-perioden = umlaufzeit ./ umlaeufe
+###############################################################################
+#                            allgemeine Rechnungen                            #
+###############################################################################
 
-omega = 2 * pi ./ perioden
+perioden = umlaufzeit ./ umlaeufe;
+
+omega = 2 * pi ./ perioden;
 
 ###############################################################################
 #                             Energiesatz Methode                             #
 ###############################################################################
 
-list_25_25 = []
-list_25_100 = []
-list_50_25 = []
-list_50_100 = []
+list_25_25 = [];
+list_25_100 = [];
+list_50_25 = [];
+list_50_100 = [];
 
 for n = 1:length(massen)
-	x = hoehen(n)
-	y = omega(n) ** 2
+	x = omega(n) ** 2;
+	y = hoehen(n);
 
-	p = [x, y]
+	p = [x, y];
 	
 	if (massen(n) == 25)
 		if (radien(n) == 10)
-			list_25_100 = [list_25_100 ; p]
+			list_25_100 = [list_25_100 ; p];
 		else
-			list_25_25 = [list_25_25 ; p]
+			list_25_25 = [list_25_25 ; p];
 		endif
 	else
 		if (radien(n) == 10)
-			list_50_100 = [list_50_100 ; p]
+			list_50_100 = [list_50_100 ; p];
 		else
-			list_50_25 = [list_50_25 ; p]
+			list_50_25 = [list_50_25 ; p];
 		endif
 	endif
 endfor
 
-save "108b_25_25.dat" list_25_25
+# Speichere Daten für Plot.
 save "108b_25_100.dat" list_25_100
+save "108b_25_25.dat" list_25_25
 save "108b_50_100.dat" list_50_100
 save "108b_50_25.dat" list_50_25
+
+function result = handleB(list, m, r)
+	x = list(:, 1);
+	y = list(:, 2);
+
+	# Geradenanpassung
+	mu = cov(x, y) / var(x);
+
+	I = m * (2 * 9.81 * mu - r^2);
+
+	result = [I];
+endfunction
+
+I_list = [ handleB(list_25_100, .025, 1) handleB(list_25_25, .025, .25) handleB(list_50_100, .050, 1) handleB(list_50_25, .050, .25) ];
+
+I_energie = mean(I_list);
+Delta_I_energie = std(I_list) / sqrt(length(I_list));
+
+epsilon_I_energie = (I_energie - I_theo) / I_theo;
+
 
 ###############################################################################
 #                            Drehmomensatz Methode                            #
 ###############################################################################
 
+list_25_25 = [];
+list_25_100 = [];
+list_50_25 = [];
+list_50_100 = [];
 
+for n = 1:length(massen)
+	x = omega(n);
+	y = fallzeiten(n);
+
+	p = [x, y];
+	
+	if (massen(n) == 25)
+		if (radien(n) == 10)
+			list_25_100 = [list_25_100 ; p];
+		else
+			list_25_25 = [list_25_25 ; p];
+		endif
+	else
+		if (radien(n) == 10)
+			list_50_100 = [list_50_100 ; p];
+		else
+			list_50_25 = [list_50_25 ; p];
+		endif
+	endif
+endfor
+
+# Speichere Daten für Plot.
+save "108c_25_100.dat" list_25_100
+save "108c_25_25.dat" list_25_25
+save "108c_50_100.dat" list_50_100
+save "108c_50_25.dat" list_50_25
+
+function result = handleC(list, m, r)
+	x = list(:, 1);
+	y = list(:, 2);
+
+	# Geradenanpassung
+	mu = cov(x, y) / var(x);
+
+	I = m * (9.81 * mu - r) * r;
+
+	result = [I];
+endfunction
+
+I_drehmoment_list = [ handleC(list_25_100, .025, 1) handleC(list_25_25, .025, .25) handleC(list_50_100, .050, 1) handleC(list_50_25, .050, .25) ];
+
+I_drehmoment = mean(I_drehmoment_list);
+Delta_I_drehmoment = std(I_drehmoment_list) / sqrt(length(I_drehmoment_list));
+
+epsilon_I_drehmoment = (I_drehmoment - I_theo) / I_theo;
+
+###############################################################################
+#                                  Aufgaben                                   #
+###############################################################################
+
+printf("\n");
+
+printf("108.b: I = %f ± %f\n", I_energie, Delta_I_energie);
+
+printf("\n");
+
+printf("108.c: I = %f ± %f\n", I_drehmoment, Delta_I_drehmoment);
+
+printf("\n");
+
+printf("108.e: epsilon_I_energie = %f\n", epsilon_I_energie);
+printf("108.e: epsilon_I_drehmoment = %f\n", epsilon_I_drehmoment);
