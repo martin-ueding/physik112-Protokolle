c43242406c959d85b1ee70905e0236ca76e3ab2c
Martin Ueding <dev@martin-ueding.de>
2013-06-22 19:54:29 +0200

    License

--- bessel.m
+++ bessel.m
@@ -1,6 +1,22 @@
 #!/usr/bin/octave -fq
 # Copyright © 2012 Martin Ueding <dev@martin-ueding.de>
 
+###############################################################################
+#                                   License                                   #
+###############################################################################
+#
+# This GNU Octave script is free software: you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or (at your
+# option) any later version.
+#
+# This is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this. If not, see <http://www.gnu.org/licenses/>.
+
 # Measurement program for the ``physik112`` experiment ``100``.
 #
 # The focal length of a thin lens is measured using the Bessel method.
